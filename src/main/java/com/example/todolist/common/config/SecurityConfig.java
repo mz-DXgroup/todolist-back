81262c7bdb7886fc57dec8e316cc817c640c4302
승현 <horang@mz.co.kr>
2024-01-02 17:41:18 +0900

    SecurityConfig 추가

---
+++ src/main/java/com/example/todolist/common/config/SecurityConfig.java
@@ -0,0 +1,32 @@
+package com.example.todolist.common.config;
+
+import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf((csrfConfig) ->
+                        csrfConfig.disable()
+                )
+                .headers((headerConfig) -> headerConfig.frameOptions(frameOptionsConfig ->
+                                frameOptionsConfig.disable()
+                        )
+                )
+                .formLogin(a -> a.defaultSuccessUrl("/"))
+                .authorizeHttpRequests((authorizeRequests) ->
+                        authorizeRequests
+                                .requestMatchers(PathRequest.toH2Console()).permitAll()
+                                .requestMatchers("/", "/login/**").permitAll()
+                                .anyRequest().authenticated()
+                );
+        return http.build();
+    }
+
+}
