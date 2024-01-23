bdec23bdaec70554954ff22364879082a94e967d
승현 <horang@mz.co.kr>
2024-01-23 09:17:42 +0900

    수정

---
+++ src/main/java/com/example/todolist/common/web/LoginController.java
@@ -0,0 +1,14 @@
+package com.example.todolist.common.web;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class LoginController {
+
+    @GetMapping("/login")
+    public String login() {
+        return "/login";
+    }
+
+}
---
+++ src/test/java/com/example/todolist/document/domain/Test2.java
@@ -0,0 +1,8 @@
+package com.example.todolist.document.domain;//package com.example.todolist.document.domain;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DocumentTest {
+
+}
