a82d896b2eb3ddaa9bd6e3a2ff780386618d8ca4
승현 <horang@mz.co.kr>
2024-02-13 15:02:49 +0900

    documentId  추가

--- src/main/java/com/example/todolist/document/application/dto/response/DocumentResponse.java
+++ src/main/java/com/example/todolist/document/application/dto/response/DocumentResponse.java
@@ -4,12 +4,14 @@ import com.example.todolist.document.domain.entity.Period;
 import com.example.todolist.document.domain.entity.Document;
 
 public record DocumentResponse(
+        Integer documentId,
         Period period,
         String title,
         String description
 ) {
     public static DocumentResponse from(Document entity) {
         return new DocumentResponse(
+                entity.getId(),
                 entity.getPeriod(),
                 entity.getTitle(),
                 entity.getDescription()
