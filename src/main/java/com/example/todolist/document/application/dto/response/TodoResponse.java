faa7a4709b8a6bb1526030a5e3bda14b89da5355
승현 <horang@mz.co.kr>
2024-02-15 09:49:16 +0900

    document 응답 documentId추가

--- src/main/java/com/example/todolist/document/application/dto/response/TodoResponse.java
+++ src/main/java/com/example/todolist/document/application/dto/response/TodoResponse.java
@@ -4,15 +4,17 @@ import com.example.todolist.document.domain.entity.Period;
 import com.example.todolist.document.domain.entity.Todo;
 
 public record TodoResponse(
+        Integer todoId,
         String todo,
         Period period,
         boolean isChecked
 ) {
     public static TodoResponse from(Todo entity){
-       return new TodoResponse(
+        return new TodoResponse(
+                entity.getId(),
                 entity.getTodo(),
                 entity.getPeriod(),
-               entity.isChecked()
+                entity.isChecked()
         );
     }
 
