9512f198594db3060c4bdcf9ec7cc7b44b1b8b89
승현 <horang@mz.co.kr>
2024-02-16 13:15:28 +0900

    Date 형식 변경

--- src/main/java/com/example/todolist/document/domain/entity/Period.java
+++ src/main/java/com/example/todolist/document/domain/entity/Period.java
@@ -2,15 +2,15 @@ package com.example.todolist.document.domain.entity;
 
 import jakarta.persistence.Embeddable;
 
-import java.time.LocalDateTime;
+import java.time.LocalDate;
 
 
 @Embeddable
 public record Period(
-        LocalDateTime startDate,
-        LocalDateTime endDate
+        LocalDate startDate,
+        LocalDate endDate
 ) {
-    private static void validate(LocalDateTime startDate, LocalDateTime endDate) {
+    private static void validate(LocalDate startDate, LocalDate endDate) {
 
         if (startDate.isAfter(endDate)) {
             throw new IllegalArgumentException("시작 시간과 끝나는 시간을 확인해 주세요");
