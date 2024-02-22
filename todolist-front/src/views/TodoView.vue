dfc2b25a90abf991941ca0e2d36db5922dca5946
Kimnayoung <nayoungk@mz.co.kr>
2024-02-22 09:03:09 +0900

    UI 수정, 오늘의 할 일 기능 추가, 파일 처리

--- todolist-front/src/views/TodoView.vue
+++ todolist-front/src/views/TodoView.vue
@@ -1,19 +1,29 @@
 <template>
   <div class="row" v-if="todo">
     <div class="col-md-4" v-for="(item, index) in todo.content" :key="index">
-      <div
-        class="card mb-3 mt-3 ms-3 me-3"
-        :class="{ 'is-checked': !item.isChecked }"
-      >
+      <div class="card mb-3 mt-3 ms-3 me-3" :class="{ 'is-checked': !item.isChecked }">
         <div class="card-body" v-if="!item.isTodoEditing">
           <strong>제목 : </strong>{{ item.todo }} <br />
           <strong>설명 : </strong>{{ item.description }} <br />
           <strong>시작일 : </strong>{{ item.period.startDate }} <br />
           <strong>종료일 : </strong>{{ item.period.endDate }} <br />
-          <strong>첨부파일 : </strong>{{ filetest.originalFilename }} <br />
-          <strong>todoId : </strong>{{ item.todoId }} <br />
-          <!-- <input type="file" @change="handleFileUpload" /> -->
-          <input type="file" @change="handleFileUpload(index, $event)" class="mt-2" />
+          <div v-if="item.fileResponses && item.fileResponses.length">
+            <strong>첨부파일 :</strong>
+            <ul>
+              <li v-for="(fileResponse, fileIndex) in item.fileResponses" :key="fileIndex">
+                {{ fileResponse.fileName }}
+                <button @click="getFileDownload(index, fileIndex)" class="btn btn-primary">↓</button>
+                <button @click="removeFile(index, fileIndex)" class="btn btn-danger ms-1">삭제</button>
+              </li>
+            </ul>
+          </div>
+
+          <input
+            type="file"
+            @change="handleFileUpload(index, $event)"
+            class="mt-2"
+          />
+          <br />
           <button @click="finishTodo(index)" class="btn btn-primary mt-3 me-1">
             완료
           </button>
@@ -42,16 +52,22 @@
       </div>
     </div>
     <div>
-      <router-link :to="{ name: 'home' }" class="router-link">home</router-link>
+      <button @click="showTodoPopup" class="btn btn-danger mt-2 ms-3">전체 삭제</button> <br />
+      <router-link :to="{ name: 'home' }" class="router-link ms-2">home</router-link>
     </div>
-    <br />
-    <div id="imagePreview">
-      <img id="img" />
+    <FormModal v-if="showTodoModal" @close="showTodoModal = false">
+      <template #header>
+        <p>확인</p>
+      </template>
+    </FormModal>
+    <div>
+      <!-- <button @click="removeTodoAll" class="btn btn-danger ms-2 mt-2">전체 삭제</button> -->
     </div>
   </div>
 </template>
 
 <script>
+import FormModal from "../components/common/FormModal.vue";
 import axios from "axios";
 import { ref } from "vue";
 import { useRoute } from "vue-router";
@@ -62,23 +78,24 @@ export default {
     const { id } = route.params;
     const todo = ref({});
     const filetest = ref({});
-    return { todo, id, filetest };
+    const showTodoModal = false;
+    return { todo, id, filetest, showTodoModal };
   },
   mounted() {
     this.getTodo();
+    // this.getFile();
   },
   methods: {
     getTodo() {
       axios.get(`http://localhost:8090/api/todos/${this.id}`).then((res) => {
         this.todo = res.data; // 객체로 저장
+        console.log(this.todo.content);
       });
     },
-    // getFile() {
-    //   axios.get(`http://localhost:8090/api/file/${}`).then((res) => {
-    //     console.log("this.id", this.id);
-    //     console.log(res);
-    //   });
-    // },
+    showTodoPopup() {
+      this.showTodoModal = !this.showTodoModal;
+      console.log(this.showTodoModal);
+    },
     handleFileUpload(index, event) {
       const test = this.todo.content[index];
       const file = event.target.files[0];
@@ -93,15 +110,12 @@ export default {
         })
         .then((response) => {
           console.log("파일 업로드 성공:", response.data);
-          console.log("아이디" ,test.todoId);
+          console.log("아이디", test.todoId);
         })
         .catch((error) => {
           console.error("파일 업로드 오류:", error);
-          console.log("아이디" ,test.todoId);
+          console.log("아이디", test.todoId);
         });
-        axios.get(`http://localhost:8090/api/file/${test.todoId}`).then((res) => {
-          this.filetest = res.data;
-      });
     },
 
     toggleTodoEditMode(index) {
@@ -109,22 +123,22 @@ export default {
     },
     saveTodoEdit(index) {
       const editedTodo = this.todo.content[index];
-      if (!editedTodo.todo.trim() || !editedTodo.description.trim()) {
-        alert("제목과 설명을 입력해주세요.");
-        return;
-      }
-      if (editedTodo.todo > 50 || editedTodo.description > 200) {
-        alert("제목은 50글자 이내, 설명은 200글자 이내로 입력해주세요.");
-        return;
-      }
-      if (!editedTodo.period.startDate || !editedTodo.period.endDate) {
-        alert("시작일과 종료일을 모두 입력해주세요.");
-        return;
-      }
-      if (editedTodo.period.startDate > editedTodo.period.endDate) {
-        alert("시작일이 종료일보다 클 수 없습니다.");
-        return;
-      }
+      // if (!editedTodo.todo.trim() || !editedTodo.description.trim()) {
+      //   alert("제목과 설명을 입력해주세요.");
+      //   return;
+      // }
+      // if (editedTodo.todo > 50 || editedTodo.description > 200) {
+      //   alert("제목은 50글자 이내, 설명은 200글자 이내로 입력해주세요.");
+      //   return;
+      // }
+      // if (!editedTodo.period.startDate || !editedTodo.period.endDate) {
+      //   alert("시작일과 종료일을 모두 입력해주세요.");
+      //   return;
+      // }
+      // if (editedTodo.period.startDate > editedTodo.period.endDate) {
+      //   alert("시작일이 종료일보다 클 수 없습니다.");
+      //   return;
+      // }
 
       axios
         .put(`http://localhost:8090/api/todo/${editedTodo.todoId}`, editedTodo)
@@ -152,6 +166,12 @@ export default {
           console.error("삭제 중 오류 발생:", error);
         });
     },
+    removeTodoAll() {
+      axios
+        .delete(`http://localhost:8090/api/todo/all/${this.id}`).then(() => {
+          console.log(this.id);
+        })
+    },
     finishTodo(index) {
       const todoToFinish = this.todo.content[index];
       todoToFinish.isChecked = !todoToFinish.isChecked;
@@ -167,6 +187,50 @@ export default {
           console.error("완료 처리 중 오류 발생:", error);
         });
     },
+    getFileDownload(todoIndex, fileIndex) {
+      const todoToDownload = this.todo.content[todoIndex];
+      const fileToDownload = todoToDownload.fileResponses[fileIndex];
+      const fileId = fileToDownload.fileId;
+      const fileName = fileToDownload.fileName;
+
+      axios({
+        url: `http://localhost:8090/api/file/download/${fileId}`,
+        method: "GET",
+        responseType: "blob",
+      })
+        .then((response) => {
+          const url = window.URL.createObjectURL(new Blob([response.data]));
+          const link = document.createElement("a");
+          link.href = url;
+          link.setAttribute("download", fileName);
+          document.body.appendChild(link);
+          link.click();
+
+          window.URL.revokeObjectURL(url);
+        })
+        .catch((error) => {
+          console.log(fileId);
+          console.error("파일 다운로드 중 오류 발생:", error);
+        });
+    },
+    removeFile(todoIndex, fileIndex) {
+      const todoToRemoveFile = this.todo.content[todoIndex];
+      const fileToRemove = todoToRemoveFile.fileResponses[fileIndex];
+      const fileId = fileToRemove.fileId;
+
+      axios
+        .delete(`http://localhost:8090/api/file/download/${fileId}`)
+        .then(() => {
+          this.todo.content[todoIndex].fileResponses.splice(fileIndex, 1);
+          console.log("파일 삭제 성공");
+        })
+        .catch((error) => {
+          console.error("파일 삭제 중 오류 발생:", error);
+        });
+    },
+  },
+  components: {
+    FormModal: FormModal,
   },
 };
 </script>
