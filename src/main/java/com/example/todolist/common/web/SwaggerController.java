584d83a346c020e89cb697bf85c1ffd9d1f671d0
LimChaeyeon <cylim@mz.co.kr>
2024-03-07 10:26:10 +0900

    생성된 restdocs, swagger 에 접근하는 controller 수정

--- src/main/java/com/example/todolist/common/web/SwaggerController.java
+++ src/main/java/com/example/todolist/common/web/SwaggerController.java
@@ -2,11 +2,18 @@ package com.example.todolist.common.web;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
-
+@RequestMapping("/docs")
 @Controller
 public class SwaggerController {
-    @GetMapping("/docs/swagger")
+
+    @GetMapping("/restdocs")
+    public String restdocs() {
+        return "forward:/restdocs/index.html";
+    }
+
+    @GetMapping("/swagger")
     public String swagger() {
         return "redirect:/swagger/index.html";
     }
